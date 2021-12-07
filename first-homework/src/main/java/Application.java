9e7a9af7b9900d8c40bdaecaee18f7d6c890064b
sbodur <sbodur@mail.com>
2021-12-07 22:47:51 +0300

    Kategori and Urun entities were created and application main was implemented

---
+++ first-homework/src/main/java/Application.java
@@ -0,0 +1,15 @@
+import entity.Kategori;
+import entityservice.KategoriEntityService;
+
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        KategoriEntityService service = new KategoriEntityService();
+        List<Kategori> kategoriList = service.findAll();
+
+        for (Kategori kategori : kategoriList) {
+            System.out.println(kategori.getAdi());
+        }
+    }
+}
